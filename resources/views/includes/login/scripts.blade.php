43d00d4324a208d53ff8faaa33a52c4dd39ea53d
luciosblack <luciano.junior@live.com>
2017-07-25 17:37:39 -0300

    Telas de logins divididas em includes

--- resources/views/includes/login/scripts.blade.php
+++ resources/views/includes/login/scripts.blade.php
@@ -10,5 +10,22 @@ $(function(){
 	.mouseout(function(){
 		$(this).find('i').removeClass('animated girar')
 	});
+
+	// Trocar o tipo de acesso
+
+	$('.troca-login-municipe').click(function(){
+			$('#login-municipe').fadeOut(1000)
+			$('#login-servidor').fadeIn(3000)
+			$('body').removeClass('fundo_roxo').addClass('fundo_dourado')
+	
+	});
+
+	$('.troca-login-servidor').click(function(){
+			$('#login-servidor').fadeOut(3000)
+			$('#login-municipe').fadeIn(1000)
+			$('body').removeClass('fundo_dourado').addClass('fundo_roxo')
+
+	});
+
 });
 </script>
--- resources/views/includes/login/timeline.blade.php
+++ resources/views/includes/login/timeline.blade.php
@@ -1,37 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<div id="login-municipe">
+        <nav class="navbar navbar-default navbar-static-top">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>360| logon do munícipe</title>
-
-    <!-- Styles -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-    <!-- Bootstrap core CSS     -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
-    <!--  Material Dashboard CSS    -->
-    <link href="css/material-dashboard.css" rel="stylesheet" />
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="css/demo.css" rel="stylesheet" />
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
-
-    <link href="{{ asset('css/animate.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
-</head>
-<body class="fundo_roxo">
-        <nav class="navbar navbar-default navbar-static-top animated fadeInDownBig">
             <div class="container">
                 <div class="navbar-header rodar_icone">
-                    <a class="navbar-brand btn btn-roxo btn-simple btn-wd btn-lg" href="{{ url('/') }}">
+                    <a class="navbar-brand btn btn-roxo btn-simple btn-wd btn-lg  troca-login-municipe" href="#">
                         <i class="material-icons">cached</i>
                         Alterar para servidor
                     </a>
@@ -84,7 +56,7 @@
                         <div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
                             <form method="#" action="#">
                                 <div class="card card-login card-hidden">
-                                <div class="logo logo-login-pn"></div>
+                                <div class="logo-roxo logo-pn"></div>
                                 <div class="card-header text-center" data-background-color="roxo">
                                         <h4 class="card-title">Login</h4>
                                         <div class="social-line">
@@ -142,61 +114,4 @@
             </footer>
         </div>
     </div>
-<!--   Core JS Files   -->
-    <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
-    <script src="js/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="js/material.min.js" type="text/javascript"></script>
-    <script src="js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
-    <!-- Forms Validations Plugin -->
-    <script src="js/jquery.validate.min.js"></script>
-    <!--  Plugin for Date Time Picker and Full Calendar Plugin-->
-    <script src="js/moment.min.js"></script>
-    <!--  Charts Plugin -->
-    <script src="js/chartist.min.js"></script>
-    <!--  Plugin for the Wizard -->
-    <script src="js/jquery.bootstrap-wizard.js"></script>
-    <!--  Notifications Plugin    -->
-    <script src="js/bootstrap-notify.js"></script>
-    <!-- DateTimePicker Plugin -->
-    <script src="js/bootstrap-datetimepicker.js"></script>
-    <!-- Vector Map plugin -->
-    <script src="js/jquery-jvectormap.js"></script>
-    <!-- Sliders Plugin -->
-    <script src="js/nouislider.min.js"></script>
-    <!--  Google Maps Plugin    -->
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <!-- Select Plugin -->
-    <script src="js/jquery.select-bootstrap.js"></script>
-    <!--  DataTables.net Plugin    -->
-    <script src="js/jquery.datatables.js"></script>
-    <!-- Sweet Alert 2 plugin -->
-    <script src="js/sweetalert2.js"></script>
-    <!--    Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-    <script src="js/jasny-bootstrap.min.js"></script>
-    <!--  Full Calendar Plugin    -->
-    <script src="js/fullcalendar.min.js"></script>
-    <!-- TagsInput Plugin -->
-    <script src="js/jquery.tagsinput.js"></script>
-    <!-- Material Dashboard javascript methods -->
-    <script src="js/material-dashboard.js"></script>
-    <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-    <script src="js/demo.js"></script>
-    <script type="text/javascript">
-        $().ready(function() {
-            demo.checkFullPageBackgroundImage();
-
-            setTimeout(function() {
-                // after 1000 ms we add the class animated to the login/register card
-                $('.card').removeClass('card-hidden');
-            }, 700)
-        });
-    </script>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-
-    @include('includes.login.scripts')
-
-</body>
-</html>
+</div> {{-- FIM LOGIN-MUNICIPE --}}
