5fd098028e2bc9af9d2bcf0b08d3c5249988439d
Filipe Molina <filipemolina@live.com>
2017-10-02 08:52:45 -0300

    Liberando a rota index de solicitações para acesso público

--- app/Http/Controllers/Api/SolicitacoesController.php
+++ app/Http/Controllers/Api/SolicitacoesController.php
@@ -12,7 +12,7 @@ class SolicitacoesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api', ['except' => ['index']]);
     }
 
     /**
