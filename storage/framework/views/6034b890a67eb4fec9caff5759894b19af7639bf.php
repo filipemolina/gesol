b89ebd2174913dc7b0bb48a34077b2bc0b1e03ba
Filipe Molina <filipemolina@live.com>
2017-11-28 15:06:56 -0200

    Bla

---
+++ storage/framework/views/6034b890a67eb4fec9caff5759894b19af7639bf.php
@@ -0,0 +1,16 @@
+<div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="card card-stats">
+        <div class="card-header" data-background-color="rose">
+            <i class="material-icons">equalizer</i>
+        </div>
+        <div class="card-content">
+            <p class="category">Website Visits</p>
+            <h3 class="card-title">75.521</h3>
+        </div>
+        <div class="card-footer">
+            <div class="stats">
+                <i class="material-icons">local_offer</i> Tracked from Google Analytics
+            </div>
+        </div>
+    </div>
+</div>
---
+++ storage/framework/views/6cd6fdcc83c0f54ab68f8b395e4b66a3f4d55d6d.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right" style="font-size: 13px">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            Equipe de Desenvolvimento de Sistemas<br>
+            <a href="http://tecnologia.mesquita.rj.gov.br" target="_blank" >
+                Subsecretaria da Tecnologia da Informação - Prefeitura Municipal de Mesquita - RJ<br>
+            </a>
+            Rua Arthur Oliveira Vecchi, 120 -  Centro - Mesquita - RJ - CEP: 26553-080 
+        </p>
+    </div>
+</footer>
