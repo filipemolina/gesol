c46d185df9ea796cfff97e2ea77dfa03e30c2d35
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2017-12-20 17:04:53 -0200

    alteração url cadastro funcionario

--- public/js/functions.js
+++ public/js/functions.js
@@ -353,7 +353,7 @@ function carrega_select_setor_edit(secretaria_id, setor_id){
 // Carrega select de setor na página de edição de funcionário
 function carrega_select_setor_create(secretaria_id, setor_id){
 
-  $.get('/setor?secretaria='+secretaria_id, function(res){
+  $.get(url_base+'/setor?secretaria='+secretaria_id, function(res){
 
     let resposta = JSON.parse(res);
     console.log(resposta);     
