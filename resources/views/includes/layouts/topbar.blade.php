85e126d7d43659cab1a64a458ca3d24646626144
Leandroaccioly <leandro.accioly@hotmail.com>
2017-12-22 16:06:15 -0200

    Com erro no topbar

--- resources/views/includes/layouts/topbar.blade.php
+++ resources/views/includes/layouts/topbar.blade.php
@@ -1,5 +1,4 @@
 <nav class="navbar navbar-absolute" style="background: #ad9a75;">
-
    <div class="container-fluid">
       <div class="navbar-minimize">
          <button id="minimizeSidebar" class="btn btn-round btn-white btn-fill btn-just-icon">
@@ -32,34 +31,41 @@
                   <p class="hidden-lg hidden-md">Dashboard</p>
                </a>
             </li>
-<!--             <li class="dropdown">
-   <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-      <i class="material-icons">notifications</i>
-      <span class="notification">5</span>
-      <p class="hidden-lg hidden-md">
-         Notifications
-         <b class="caret"></b>
-      </p>
-   </a>
-   <ul class="dropdown-menu">
-      <li>
-         <a href="#">Mike John responded to your email</a>
-      </li>
-      <li>
-         <a href="#">You have 5 new tasks</a>
-      </li>
-      <li>
-         <a href="#">You're now friend with Andrew</a>
-      </li>
-      <li>
-         <a href="#">Another Notification</a>
-      </li>
-      <li>
-         <a href="#">Another One</a>
-      </li>
-   </ul>
-</li>  -->
-
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="{{ url("/") }}" class="dropdown-toggle" data-toggle="dropdown" style="color: #000000; font-weight: bold;">
+                        <i class="material-icons">dashboard</i>
+                        <p class="hidden-lg hidden-md">Dashboard</p>
+                    </a>
+                </li>
+               {{--  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="material-icons">notifications</i>
+                        <span class="notification">5</span>
+                        <p class="hidden-lg hidden-md">
+                            Notifications
+                            <b class="caret"></b>
+                        </p>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#">Mike John responded to your email</a>
+                        </li>
+                        <li>
+                            <a href="#">You have 5 new tasks</a>
+                        </li>
+                        <li>
+                            <a href="#">You're now friend with Andrew</a>
+                        </li>
+                        <li>
+                            <a href="#">Another Notification</a>
+                        </li>
+                        <li>
+                            <a href="#">Another One</a>
+                        </li>
+                    </ul>
+                </li> --}}
                 <li class="dropdown">
                     <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown" style="color: #000000; font-weight: bold">
                         <i class="material-icons">person</i>
